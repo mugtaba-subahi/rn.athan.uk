8983ab394bd1c14c19bec3bf271a9757a93375bd
mugtaba <49446523+mugtaba-subahi@users.noreply.github.com>
2024-11-07 17:43:27 +0000

    removed overlay

--- store/store.ts
+++ store/store.ts
@@ -1,6 +1,5 @@
 import { atom } from 'jotai';
 import { ITransformedToday } from '@/types/prayers';
-import { COLORS } from '@/constants';
 
 export const isLoadingAtom = atom<boolean>(true);
 export const hasErrorAtom = atom<boolean>(false);
@@ -9,7 +8,3 @@ export const todaysPrayersAtom = atom<ITransformedToday>({});
 export const tomorrowsPrayersAtom = atom<ITransformedToday>({});
 export const nextPrayerIndexAtom = atom<number>(-1);
 export const lastValidPositionAtom = atom<number>(0);
-
-export const backgroundColorsAtom = atom((get) => {
-  return [COLORS.gradientStart, COLORS.gradientEnd];
-});
