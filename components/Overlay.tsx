87a4a64a1c2a64694a131451192c90305808384a
mugtaba <49446523+mugtaba-subahi@users.noreply.github.com>
2024-11-10 04:24:13 +0000

    removed overlay

--- components/Overlay.tsx
+++ components/Overlay.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Pressable, View } from 'react-native';
+import { StyleSheet, Pressable, View, Dimensions } from 'react-native';
 import Reanimated from 'react-native-reanimated';
 import { Portal } from 'react-native-paper';
 import { useAtom } from 'jotai';
@@ -9,42 +9,33 @@ import { useEffect } from 'react';
 import {
   useSharedValue,
   withTiming,
-  useAnimatedProps,
-  useAnimatedStyle
+  useAnimatedProps
 } from 'react-native-reanimated';
+import RadialGlow from './RadialGlow';
 
 const AnimatedBlur = Reanimated.createAnimatedComponent(BlurView);
-const AnimatedGradient = Reanimated.createAnimatedComponent(LinearGradient);
 
 export default function Overlay() {
-  const [visible, setVisible] = useAtom(overlayVisibleAtom);
+  const [overlayVisible, setOverlayVisible] = useAtom(overlayVisibleAtom);
   const [content, setOverlayContent] = useAtom(overlayContentAtom);
   const [, setSelectedPrayerIndex] = useAtom(selectedPrayerIndexAtom);
 
   const intensity = useSharedValue(0);
-  const gradientOpacity = useSharedValue(0);
 
   const animatedProps = useAnimatedProps(() => ({
     intensity: intensity.value,
   }));
 
-  const animatedGradientStyle = useAnimatedStyle(() => ({
-    opacity: gradientOpacity.value
-  }));
-
   useEffect(() => {
-    if (visible) {
+    if (overlayVisible) {
       intensity.value = withTiming(10, { duration: 300 });
-      gradientOpacity.value = withTiming(1, { duration: 300 });
     }
-  }, [visible]);
+  }, [overlayVisible]);
 
   const handleClose = () => {
     setSelectedPrayerIndex(-1);
     intensity.value = withTiming(0, { duration: 300 });
-    gradientOpacity.value = withTiming(0, { duration: 300 });
-
-    setVisible(false);
+    setOverlayVisible(false);
     setOverlayContent([]);
   };
 
@@ -55,16 +46,20 @@ export default function Overlay() {
     return acc;
   }, [] as typeof content);
 
-  if (!visible) return null;
+  if (!overlayVisible) return null;
 
   return (
     <Portal>
       <AnimatedBlur animatedProps={animatedProps} tint="light" style={StyleSheet.absoluteFill}>
-        <AnimatedGradient
+        <LinearGradient
           colors={['rgba(25,0,40,1)', 'rgba(8,0,12,0.9)', 'rgba(0,0,0,1)']}
-          style={[StyleSheet.absoluteFill, animatedGradientStyle]}
+          style={StyleSheet.absoluteFill}
           start={{ x: 0, y: 0 }}
           end={{ x: 0, y: 1 }}
+        />
+        <RadialGlow
+          size={Dimensions.get('window').height / 3}
+          style={styles.radialGradient}
         />
         <Pressable style={styles.overlay} onPress={handleClose}>
           {uniqueContent.map(({ name, component, measurements }) => (
@@ -97,6 +92,14 @@ const styles = StyleSheet.create({
     zIndex: 1000,
   },
   content: {
-    zIndex: 1001,
+    zIndex: 1002,
+  },
+  radialGradient: {
+    position: 'absolute',
+    top: -Dimensions.get('window').height / 2,  // Move up by half the height
+    left: -Dimensions.get('window').height / 10,  // Move left by half the width
+    width: Dimensions.get('window').height / 1,
+    height: Dimensions.get('window').height / 1,
+    zIndex: 1001,  // between overlay and content
   }
 });
