7cf10a5eb6b1a4331e3da8461bf3694298456966
mugtaba <49446523+mugtaba-subahi@users.noreply.github.com>
2024-11-13 04:43:47 +0000

    overlay transitions

--- components/Prayer.tsx
+++ components/Prayer.tsx
@@ -100,14 +100,8 @@ export default function Prayer({ index, isOverlay = false }: Props) {
       <Animated.Text style={[styles.text, styles.arabic, animatedTextStyle]}>
         {prayer.arabic}
       </Animated.Text>
-      <PrayerTime
-        index={index}
-        isOverlay={isOverlay}
-      />
-      <Alert
-        index={index}
-        isOverlay={isOverlay}
-      />
+      <PrayerTime index={index} isOverlay={isOverlay} />
+      <Alert index={index} isOverlay={isOverlay} />
     </AnimatedPressable>
   );
 }
