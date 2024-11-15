1bd70d6ccda5b4da9b8d393a8b3898bcb3c24474
mugtaba <49446523+mugtaba-subahi@users.noreply.github.com>
2024-11-15 18:49:49 +0000

    overlay transitions

--- components/ActiveBackground.tsx
+++ components/ActiveBackground.tsx
@@ -1,25 +1,56 @@
+import { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { useAtom } from 'jotai';
-import Animated, { useAnimatedStyle, withSpring, withTiming } from 'react-native-reanimated';
-import { nextPrayerIndexAtom, absolutePrayerMeasurementsAtom, overlayVisibleAtom } from '@/store/store';
+import Animated, {
+  useAnimatedStyle,
+  withSpring,
+  withTiming,
+  useSharedValue,
+  runOnUI,
+  runOnJS
+} from 'react-native-reanimated';
+import {
+  nextPrayerIndexAtom,
+  absolutePrayerMeasurementsAtom,
+  dateAtom,
+  todaysPrayersAtom,
+} from '@/store/store';
 import { ANIMATION, COLORS, OVERLAY, PRAYER } from '@/constants';
+import { getTodayOrTomorrowDate } from '@/utils/time';
 
+const TIMING_CONFIG = { duration: ANIMATION.overlayDelay };
 const SPRING_CONFIG = { damping: 10, stiffness: 100, mass: 1 };
 
 export default function ActiveBackground() {
   const [nextPrayerIndex] = useAtom(nextPrayerIndexAtom);
   const [absoluteMeasurements] = useAtom(absolutePrayerMeasurementsAtom);
+  const [date] = useAtom(dateAtom);
+  const [todaysPrayers] = useAtom(todaysPrayersAtom);
+
+  const opacityShared = useSharedValue(0);
+  const backgroundColorShared = useSharedValue(COLORS.activeBackground);
+
+  useEffect(() => {
+    if (!absoluteMeasurements[nextPrayerIndex]) return;
+
+    const nowDate = getTodayOrTomorrowDate('today');
+    const lastPrayerIndex = 6;
+    const isActive = date === nowDate && !todaysPrayers[lastPrayerIndex]?.passed;
+    const isActiveOpacity = isActive ? 1 : 0.1;
+    const isActiveBackgroundColor = isActive ? COLORS.activeBackground : COLORS.inactiveBackground;
+
+    opacityShared.value = withTiming(isActiveOpacity, TIMING_CONFIG);
+    backgroundColorShared.value = withTiming(isActiveBackgroundColor, TIMING_CONFIG);
+  }, [absoluteMeasurements, nextPrayerIndex, date, todaysPrayers]);
 
   const animatedStyle = useAnimatedStyle(() => {
-    if (nextPrayerIndex === -1 || !absoluteMeasurements[nextPrayerIndex]) {
-      return { opacity: withTiming(0, { duration: ANIMATION.overlayDelay }) };
-    }
+    if (!absoluteMeasurements[nextPrayerIndex]) return { opacity: opacityShared.value };
 
     const activePrayer = absoluteMeasurements[nextPrayerIndex];
 
     return {
-      // opacity: withTiming(0.1, { duration: ANIMATION.overlayDelay }),
-      opacity: withTiming(1, { duration: ANIMATION.overlayDelay }),
+      opacity: opacityShared.value,
+      backgroundColor: backgroundColorShared.value,
       position: 'absolute',
       top: withSpring(activePrayer.pageY, SPRING_CONFIG),
       left: activePrayer.pageX,
@@ -35,8 +66,6 @@ export default function ActiveBackground() {
 const styles = StyleSheet.create({
   background: {
     position: 'absolute',
-    backgroundColor: COLORS.activeBackground,
-    // backgroundColor: COLORS.inactiveBackground,
     borderRadius: PRAYER.borderRadius,
     shadowColor: COLORS.primaryShadow,
     shadowOffset: { width: 0, height: 10 },
