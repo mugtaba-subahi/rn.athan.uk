f23b2649b71ee7702a03087e4f1aebf98f54ca79
mugtaba <49446523+mugtaba-subahi@users.noreply.github.com>
2024-11-18 14:16:49 +0000

    overlay transitions

--- hooks/useCountdown.ts
+++ hooks/useCountdown.ts
@@ -1,14 +1,9 @@
 import { useState, useEffect } from 'react';
-import { useAtom } from 'jotai';
-import Store from '@/stores/store';
-import { DaySelection, PrayerType } from '@/shared/types';
+import { DaySelection } from '@/shared/types';
 import { getTimeDifference, getRecentDate, formatTime } from '@/shared/time';
-import { PRAYERS_ENGLISH } from '@/shared/constants';
 import useSchedule from '@/hooks/useSchedule';
 
-const THRESHOLD = 1000; // seconds
-
-export const useCountdown = (type: PrayerType) => {
+export const useCountdown = () => {
   const { today, tomorrow, nextIndex } = useSchedule('standard');
   
   const [countdown, setCountdown] = useState('');
@@ -19,13 +14,13 @@ export const useCountdown = (type: PrayerType) => {
       const prayer = today[nextIndex];
 
       const diff = getTimeDifference(prayer.time, getRecentDate(DaySelection.Today));
-
       setCountdown(formatTime(diff));
     };
 
     // Update countdown every second
     updateCountdown();
     const intervalId = setInterval(updateCountdown, 1000);
+    
     return () => clearInterval(intervalId);
   }, [today, tomorrow, nextIndex]);
 
