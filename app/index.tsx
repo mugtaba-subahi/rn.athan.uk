16ece0829c4476163db300ef2273856eacaf75f2
mugtaba <49446523+mugtaba-subahi@users.noreply.github.com>
2024-11-23 18:41:45 +0000

    overlay transitions

--- app/index.tsx
+++ app/index.tsx
@@ -1,67 +1,22 @@
-import { useEffect, useMemo } from 'react';
+import { useEffect } from 'react';
 import { StyleSheet, StatusBar } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
 import { useFonts } from 'expo-font';
 import { WaveIndicator } from 'react-native-indicators';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import Animated, {
-  useAnimatedStyle,
-  interpolateColor,
-  useDerivedValue,
-  withSpring
-} from 'react-native-reanimated';
-import { useAtomValue } from 'jotai';
-import { pagePositionAtom } from '@/stores/store';
-
-// Wrap LinearGradient with Animated
-const AnimatedLinearGradient = Animated.createAnimatedComponent(LinearGradient);
 
 import { setSchedule, setDate } from '@/stores/actions';
-import { COLORS, OVERLAY } from '@/shared/constants';
 import Navigation from '@/app/Navigation';
 import RadialGlow from '@/components/RadialGlow';
 import * as Database from '@/stores/database';
 import * as Api from '@/api/client';
 import { ScheduleType } from '@/shared/types';
+import GradientBackground from '@/components/GradientBackground';
 
 export default function Index() {
   const [fontsLoaded] = useFonts({
     'Roboto': require('@/assets/fonts/Roboto-Regular.ttf'),
     'Roboto-Medium': require('@/assets/fonts/Roboto-Medium.ttf')
   });
-
-  const position = useAtomValue(pagePositionAtom);
-
-  const colors = useMemo(() => ({
-    start: [COLORS.gradientScreen1Start, COLORS.gradientScreen2Start],
-    end: [COLORS.gradientScreen1End, COLORS.gradientScreen2End],
-  }), []);
-
-  const animatedPosition = useDerivedValue(() => {
-    return withSpring(position, {
-      damping: 20,
-      stiffness: 90,
-      mass: 0.5
-    });
-  });
-
-  const gradientStyle = useAnimatedStyle(() => {
-    'worklet';
-    const startColor = interpolateColor(
-      animatedPosition.value,
-      [0, 1],
-      colors.start
-    );
-    const endColor = interpolateColor(
-      animatedPosition.value,
-      [0, 1],
-      colors.end
-    );
-
-    return {
-      colors: [startColor, endColor]
-    };
-  }, []);
 
   useEffect(() => {
     const init = async () => {
@@ -83,20 +38,7 @@ export default function Index() {
 
   return (
     <GestureHandlerRootView style={StyleSheet.absoluteFillObject}>
-      <AnimatedLinearGradient
-        animatedProps={gradientStyle}
-        style={[
-          StyleSheet.absoluteFillObject,
-          {
-            zIndex: OVERLAY.zindexes.background,
-          }
-        ]}
-        shouldRasterizeIOS={true}
-        needsOffscreenAlphaCompositing={true}
-        renderToHardwareTextureAndroid={true}
-        start={{ x: 0, y: 0 }}
-        end={{ x: 1, y: 1 }}
-      />
+      <GradientBackground />
       <StatusBar barStyle="light-content" />
       <RadialGlow />
       <Navigation />
---
+++ components/GradientBackground.tsx
@@ -0,0 +1,54 @@
+
+import { StyleSheet, useWindowDimensions } from 'react-native';
+import { useAtomValue } from 'jotai';
+import { pagePositionAtom } from '@/stores/store';
+import { Canvas, LinearGradient, Rect, vec } from '@shopify/react-native-skia';
+import { useAnimatedStyle, interpolateColor, useDerivedValue, withSpring, useSharedValue } from 'react-native-reanimated';
+import { useMemo, useEffect } from 'react';
+import { COLORS, OVERLAY } from '@/shared/constants';
+
+export default function GradientBackground() {
+  const { width, height } = useWindowDimensions();
+  const position = useAtomValue(pagePositionAtom);
+
+  const colors = useMemo(() => ({
+    start: [COLORS.gradientScreen1Start, COLORS.gradientScreen2Start],
+    end: [COLORS.gradientScreen1End, COLORS.gradientScreen2End],
+  }), []);
+
+  const animatedPosition = useDerivedValue(() => {
+    return withSpring(position, {
+      damping: 20,
+      stiffness: 90,
+      mass: 0.5
+    });
+  });
+
+  const startColorValue = useSharedValue(colors.start[0]);
+  const endColorValue = useSharedValue(colors.end[0]);
+
+  useEffect(() => {
+    startColorValue.value = interpolateColor(
+      position,
+      [0, 1],
+      colors.start
+    );
+    endColorValue.value = interpolateColor(
+      position,
+      [0, 1],
+      colors.end
+    );
+  }, [position]);
+
+  return (
+    <Canvas style={[StyleSheet.absoluteFillObject, { zIndex: OVERLAY.zindexes.background }]}>
+      <Rect x={0} y={0} width={width} height={height}>
+        <LinearGradient
+          start={vec(0, 0)}
+          end={vec(width, height)}
+          colors={[startColorValue.value, endColorValue.value]}
+        />
+      </Rect>
+    </Canvas>
+  );
+}
