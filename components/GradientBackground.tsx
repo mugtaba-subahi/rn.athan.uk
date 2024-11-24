99d42a450ad13c52c4928743567a0ed606498c9b
mugtaba <49446523+mugtaba-subahi@users.noreply.github.com>
2024-11-24 23:38:38 +0000

    overlay transitions

--- components/GradientBackground.tsx
+++ components/GradientBackground.tsx
@@ -1,15 +1,16 @@
-
 import { StyleSheet, useWindowDimensions } from 'react-native';
 import { useAtomValue } from 'jotai';
 import { pagePositionAtom } from '@/stores/store';
 import { Canvas, LinearGradient, Rect, vec } from '@shopify/react-native-skia';
 import { interpolateColor, useSharedValue } from 'react-native-reanimated';
-import { useMemo, useEffect } from 'react';
+import { useMemo, useEffect, useState } from 'react';
 import { COLORS } from '@/shared/constants';
 
 export default function GradientBackground() {
   const { width, height } = useWindowDimensions();
   const position = useAtomValue(pagePositionAtom);
+  const [currentStart, setCurrentStart] = useState(COLORS.gradientScreen1Start);
+  const [currentEnd, setCurrentEnd] = useState(COLORS.gradientScreen1End);
 
   const colors = useMemo(() => ({
     start: [COLORS.gradientScreen1Start, COLORS.gradientScreen2Start],
@@ -22,6 +23,8 @@ export default function GradientBackground() {
   useEffect(() => {
     startColorValue.value = interpolateColor(position, [0, 1], colors.start);
     endColorValue.value = interpolateColor(position, [0, 1], colors.end);
+    setCurrentStart(startColorValue.value);
+    setCurrentEnd(endColorValue.value);
   }, [position]);
 
   return (
@@ -30,7 +33,7 @@ export default function GradientBackground() {
         <LinearGradient
           start={vec(0, 0)}
           end={vec(width, height)}
-          colors={[startColorValue.value, endColorValue.value]}
+          colors={[currentStart, currentEnd]}
         />
       </Rect>
     </Canvas>
