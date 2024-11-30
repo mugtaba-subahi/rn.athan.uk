72525b0ea3b9b6217c9a656be31786c79c6bbb7f
mugtaba <49446523+mugtaba-subahi@users.noreply.github.com>
2024-11-30 18:12:08 +0000

    yes

--- components/Glow.tsx
+++ components/Glow.tsx
@@ -5,14 +5,14 @@ import Svg, { RadialGradient, Stop, Circle } from 'react-native-svg';
 const AnimatedSvg = Reanimated.createAnimatedComponent(Svg);
 
 export default function Glow() {
-  const baseOpacity = 0.5;
+  const baseOpacity = 0.75;
   const color = 'rgb(128,0,255)';
   const size = Dimensions.get('window').width * 1.5;
 
   return (
     <AnimatedSvg width={size} height={size} viewBox={`0 0 ${size} ${size}`} style={[styles.glow]}>
       <RadialGradient id="radialGlow" cx="50%" cy="50%" r="50%" fx="50%" fy="50%">
-        <Stop offset="0%" stopColor={color} stopOpacity={baseOpacity * 0.75} />
+        <Stop offset="0%" stopColor={color} stopOpacity={baseOpacity * 0.5} />
         <Stop offset="35%" stopColor={color} stopOpacity={baseOpacity * 0.4} />
         <Stop offset="65%" stopColor={color} stopOpacity={baseOpacity * 0.15} />
         <Stop offset="85%" stopColor={color} stopOpacity={baseOpacity * 0.05} />
@@ -26,7 +26,7 @@ export default function Glow() {
 const styles = StyleSheet.create({
   glow: {
     position: 'absolute',
-    bottom: -Dimensions.get('window').width / 1.25,
+    top: -Dimensions.get('window').width / 1.25,
     left: -Dimensions.get('window').width / 2,
     pointerEvents: 'none',
   },
