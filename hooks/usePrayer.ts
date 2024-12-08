c94a51f264a5d20ad6128f0db24eefe7177cd319
mugtaba <49446523+mugtaba-subahi@users.noreply.github.com>
2024-12-08 02:38:06 +0000

    yes

--- hooks/usePrayer.ts
+++ hooks/usePrayer.ts
@@ -10,10 +10,15 @@ export const usePrayer = (type: ScheduleType, index: number = 0) => {
 
   const overlay = useAtomValue(overlayAtom);
 
-  const prayer = schedule.today[index];
-  const isPassed = TimeUtils.isTimePassed(prayer.time);
+  const todayPrayer = schedule.today[index];
+  const tomorrowPrayer = schedule.tomorrow[index];
+
+  const isPassed = TimeUtils.isTimePassed(todayPrayer.time);
   const isNext = index === schedule.nextIndex;
   const isOnOverlay = overlay.isOn && overlay.selectedPrayerIndex === index && overlay.scheduleType === type;
+
+  // Use tomorrow's prayer if the current prayer is passed and shown in overlay
+  const prayer = isPassed && isOnOverlay ? tomorrowPrayer : todayPrayer;
 
   const ui = {
     initialColorPos: isPassed || isNext ? 1 : 0,
