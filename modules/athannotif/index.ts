5856926e31fa83d9803e768745ea01c5897646bd
mugtaba <49446523+mugtaba-subahi@users.noreply.github.com>
2024-12-13 12:45:07 +0000

    yes

--- modules/athannotif/index.ts
+++ modules/athannotif/index.ts
@@ -1,10 +1,6 @@
-import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
-
 // Import the native module. On web, it will be resolved to Athannotif.web.ts
 // and on native platforms to Athannotif.ts
-import { ChangeEventPayload, AthannotifViewProps } from './src/Athannotif.types';
 import AthannotifModule from './src/AthannotifModule';
-import AthannotifView from './src/AthannotifView';
 
 // Get the native constant value.
 export const PI = AthannotifModule.PI;
@@ -12,15 +8,3 @@ export const PI = AthannotifModule.PI;
 export function hello(): string {
   return AthannotifModule.hello();
 }
-
-export async function setValueAsync(value: string) {
-  return await AthannotifModule.setValueAsync(value);
-}
-
-const emitter = new EventEmitter(AthannotifModule ?? NativeModulesProxy.Athannotif);
-
-export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
-  return emitter.addListener<ChangeEventPayload>('onChange', listener);
-}
-
-export { AthannotifView, AthannotifViewProps, ChangeEventPayload };
