d08621fedb41ca07cce08de15d633ab2ae9774e2
mugtaba <49446523+mugtaba-subahi@users.noreply.github.com>
2024-12-13 14:44:46 +0000

    yes

--- modules/athannotif/ios/AthannotifDelegate.swift
+++ modules/athannotif/ios/AthannotifDelegate.swift
@@ -1,18 +1,29 @@
 import ExpoModulesCore
 import UserNotifications
 
+class NotificationDelegate: NSObject, UNUserNotificationCenterDelegate {
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        print("THIS IS WORKING!!!!! - DEBUG: Notification received in foreground")
+    }
+}
+
 public class AthannotifDelegate: ExpoAppDelegateSubscriber {
+    private let notificationDelegate = NotificationDelegate()
+    
     public required init() {
         super.init()
         print("THIS WORKS - DEBUG: AthannotifDelegate initialized")
     }
     
-    // This method is called when a notification is delivered while the app is in the foreground
-    public func userNotificationCenter(
-        _ center: UNUserNotificationCenter,
-        willPresent notification: UNNotification,
-        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
-    ) {
-        print("NOT WORKING - DEBUG: Notification received in foreground")
+    public func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        UNUserNotificationCenter.current().delegate = notificationDelegate
+        return true
     }
 }
