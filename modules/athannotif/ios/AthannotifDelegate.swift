06048e4b6e2f9f3eab58c68d5f0e5297cf6c5918
mugtaba <49446523+mugtaba-subahi@users.noreply.github.com>
2024-12-13 13:57:38 +0000

    yes

--- modules/athannotif/ios/AthannotifDelegate.swift
+++ modules/athannotif/ios/AthannotifDelegate.swift
@@ -3,8 +3,8 @@ import UserNotifications
 
 // Define a class to handle local notifications
 public class AthannotifDelegate: ExpoAppDelegateSubscriber {
-    // This function is triggered when a notification is received
-    public func application(_ application: UIApplication, didReceive notification: UNNotification) {
-        print("DEBUG: Local notification received: \(notification.request.content.body)")
+    // Initializer: Log when the class is initialized
+    public required init() {
+        print("DEBUG: AthannotifDelegate initialized")
     }
 }
