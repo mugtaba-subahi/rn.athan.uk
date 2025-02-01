2b6b8db9b5bce7811249696aada090a99370f479
mugtaba <49446523+mugtaba-subahi@users.noreply.github.com>
2025-02-01 22:04:24 +0000

    yes

--- app/index.tsx
+++ app/index.tsx
@@ -7,17 +7,20 @@ import Navigation from '@/app/Navigation';
 import FontRobotoMedium from '@/assets/fonts/Roboto-Medium.ttf';
 import FontRoboto from '@/assets/fonts/Roboto-Regular.ttf';
 import Error from '@/components/Error';
+import ModalTips from '@/components/ModalTips';
 import Overlay from '@/components/Overlay';
 import { initializeListeners } from '@/device/listeners';
 import { useNotification } from '@/hooks/useNotification';
 import { initializeNotifications } from '@/shared/notifications';
 import { syncLoadable } from '@/stores/sync';
+import { popupTipAthanEnabledAtom, setPopupTipAthanEnabled } from '@/stores/ui';
 
 export default function Index() {
   useFonts({ Roboto: FontRoboto, 'Roboto-Medium': FontRobotoMedium });
 
   const { state } = useAtomValue(syncLoadable);
   const { checkInitialPermissions } = useNotification();
+  const modalTipEnabled = useAtomValue(popupTipAthanEnabledAtom);
 
   useEffect(() => {
     // Initialize notifications on first load
@@ -29,11 +32,16 @@ export default function Index() {
     // checkForUpdates();
   }, []);
 
+  const handleCloseTip = () => {
+    setPopupTipAthanEnabled(false);
+  };
+
   if (state === 'loading') return <WaveIndicator color="white" />;
   if (state === 'hasError') return <Error />;
 
   return (
     <>
+      <ModalTips visible={modalTipEnabled} onClose={handleCloseTip} />
       <Overlay />
       <Navigation />
     </>
--- stores/ui.ts
+++ stores/ui.ts
@@ -1,6 +1,8 @@
 import { BottomSheetModal } from '@gorhom/bottom-sheet';
 import { atom, getDefaultStore } from 'jotai';
 
+import { atomWithStorageBoolean } from '@/stores/storage';
+
 const store = getDefaultStore();
 
 // --- Atoms ---
@@ -8,14 +10,13 @@ export const pagePositionAtom = atom(0);
 export const bottomSheetModalAtom = atom<BottomSheetModal | null>(null);
 export const playingSoundIndexAtom = atom<number | null>(null);
 export const refreshUIAtom = atom<number>(Date.now());
+export const popupTipAthanEnabledAtom = atomWithStorageBoolean('popup_tip_athan_enabled', true);
 
 // --- Actions ---
 export const setPagePosition = (position: number) => store.set(pagePositionAtom, position);
-
 export const setBottomSheetModal = (modal: BottomSheetModal | null) => store.set(bottomSheetModalAtom, modal);
-
 export const showSheet = () => store.get(bottomSheetModalAtom)?.present();
 export const hideSheet = () => store.get(bottomSheetModalAtom)?.dismiss();
-
 export const setPlayingSoundIndex = (index: number | null) => store.set(playingSoundIndexAtom, index);
 export const setRefreshUI = (timestamp: number) => store.set(refreshUIAtom, timestamp);
+export const setPopupTipAthanEnabled = (enabled: boolean) => store.set(popupTipAthanEnabledAtom, enabled);
