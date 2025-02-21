635ba6d8a0b131961e76120e18415008e06dfbed
mugtaba <49446523+mugtaba-subahi@users.noreply.github.com>
2025-02-21 08:34:18 +0000

    added open source MIT license

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,12 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
-import { useFonts } from 'expo-font';
 import { Slot } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { LogBox } from 'react-native';
 import { SystemBars } from 'react-native-edge-to-edge';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import BottomSheetSound from '@/components/BottomSheetSound';
-import Error from '@/components/Error';
 
 // Prevent splash screen from automatically hiding
 SplashScreen.preventAutoHideAsync();
@@ -19,22 +15,9 @@ SplashScreen.preventAutoHideAsync();
 LogBox.ignoreLogs(['Require cycle']);
 
 export default function Layout() {
-  const [isReady, setIsReady] = useState(false);
-
-  const [fontsLoaded, fontError] = useFonts({
-    Roboto: require('@/assets/fonts/Roboto-Regular.ttf'),
-    'Roboto-Medium': require('@/assets/fonts/Roboto-Medium.ttf'),
-  });
-
   useEffect(() => {
-    if (!fontsLoaded) return;
-
     SplashScreen.hideAsync();
-    setIsReady(true);
-  }, [fontsLoaded]);
-
-  if (fontError) return <Error />;
-  if (!isReady) return null;
+  }, []);
 
   return (
     <GestureHandlerRootView style={{ flex: 1, backgroundColor: '#2c1c77' }}>
