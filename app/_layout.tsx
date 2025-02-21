280d1813138c3d7bbe851e21e658a7b07fefbaf7
mugtaba <49446523+mugtaba-subahi@users.noreply.github.com>
2025-02-21 22:26:27 +0000

    added open source MIT license

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,7 @@ import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { Slot } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-import { LogBox } from 'react-native';
+import { LogBox, Text } from 'react-native';
 import { SystemBars } from 'react-native-edge-to-edge';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
@@ -13,6 +13,14 @@ SplashScreen.preventAutoHideAsync();
 
 // Ignore logs
 LogBox.ignoreLogs(['Require cycle']);
+
+// Set default props for all Text components
+// @ts-expect-error silent
+Text.defaultProps = {
+  // @ts-expect-error silent
+  ...Text.defaultProps,
+  allowFontScaling: false,
+};
 
 export default function Layout() {
   useEffect(() => {
